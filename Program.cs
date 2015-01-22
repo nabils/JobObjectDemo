d73f616fefcb760522396a51e1785f635fdbd44f
Nabil Shuhaiber <nabil@shuhaiber.com>
2015-01-22 19:49:50 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace JobObjectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process =
+                Process.Start(
+                    @"C:\Users\Nabil Shuhaiber\Documents\Dev\BlockingProcess\BlockingProcess\bin\Debug\BlockingProcess.exe");
+
+            var job = new Job();
+            job.AddProcess(process.Id);
+
+            Console.WriteLine("Running... Press key to kill");
+            Console.ReadLine();
+        }
+    }
+}
